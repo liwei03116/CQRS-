50898fd508de2872b52af65d2a192fe806094baa
liwei03116 <liwei03116@hotmail.com>
2021-01-20 11:17:22 +1100

    first commit

---
+++ Cafe/Tab/OpenTab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cafe.Tab
+{
+    public class OpenTab
+    {
+        public Guid Id;
+        public int TableNumber { get; set; }
+        public string Waiter { get; set; }
+
+        public int BookNumber { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+    }
+}
---
+++ Events/Tab/TabOpened.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Events.Cafe
+{
+    public class TabOpened
+    {
+        public Guid Id;
+        public int TableNumber;
+        public string Waiter;
+
+        public int BookNumber;
+        public string Name;
+        public int Phone;
+        public string Email;
+        public DateTime CheckIn;
+        public DateTime CheckOut;
+    }
+}
